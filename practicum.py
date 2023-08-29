3f3ad1d6c27d35455f74466f659a349044baa29e
Fyodor Davidov <Fedor.Davidov@honeywell.com>
2023-08-29 16:57:59 +0500

    Подготовка к Гит Хабу

--- practicum.py
+++ practicum.py
@@ -1,1 +1,10 @@
-print('Привет, Яндекс!')
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.field_names = ['Name', 'Age', 'City']
+table.add_row(['Petr', 11, 'Moscow'])
+table.add_row(['Fyodor', 30, 'Tashkent'])
+table.add_row(['Nastya', 31, 'Tashkent'])
+
+print(table)
